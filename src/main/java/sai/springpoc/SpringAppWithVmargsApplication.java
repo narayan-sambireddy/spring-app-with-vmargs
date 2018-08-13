25d9e9371409670edf2b76ac05b59cfae73b7ab5
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-13 23:44:40 +0530

    Initial commit

---
+++ src/main/java/sai/springpoc/SpringAppWithVmargsApplication.java
@@ -0,0 +1,12 @@
+package sai.springpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWithVmargsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWithVmargsApplication.class, args);
+	}
+}
